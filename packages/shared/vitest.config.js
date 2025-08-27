bec942066a299aed0b81bc04c900e405da0fd627
Alejandro Aspinwall <aaspinwall@gmail.com>
2025-08-27 12:44:01 -0400

    add prettier and eslint

--- packages/shared/vitest.config.js
+++ packages/shared/vitest.config.js
@@ -3,19 +3,19 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@shared': path.resolve(__dirname, 'src'),
-        },
+  resolve: {
+    alias: {
+      '@shared': path.resolve(__dirname, 'src'),
     },
-    test: {
-        // this is a temporary config to use vite on routes tests
-        include: ['**/tests/*.test.ts'],
-        exclude: ['**/node_modules/**'],
-        environment: 'node',
-        setupFiles: ['dotenv/config'],
-        env: {
-            ...config({ path: './env' }).parsed,
-        },
+  },
+  test: {
+    // this is a temporary config to use vite on routes tests
+    include: ['**/tests/*.test.ts'],
+    exclude: ['**/node_modules/**'],
+    environment: 'node',
+    setupFiles: ['dotenv/config'],
+    env: {
+      ...config({ path: './env' }).parsed,
     },
+  },
 });
