5a081faeabdc67bbb8ee9a4ffb94c9eadc17316e
Alejandro Aspinwall <aaspinwall@gmail.com>
2025-08-27 15:24:05 -0400

    remove modules

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,29 +3,11 @@ import logger from './logger';
 
 // Import the tsconfig base to ensure it gets copied to dist during compilation
 import tsconfigBase from '../tsconfig.client.base.json';
-import { useAuth } from './auth/auth';
-import * as validations from './lib/validations';
-import { default as useApiStore } from './stores/api-store';
-import { useAuthStore } from './stores/auth-store';
-import * as types from './types';
 
-export {
-  logger,
-  tsconfigBase,
-  types,
-  useApiStore,
-  useAuth,
-  useAuthStore,
-  validations,
-};
+export { logger, tsconfigBase };
 
 // add a default export to avoid issues with some bundlers
 export default {
   logger,
   tsconfigBase,
-  types,
-  useApiStore,
-  useAuth,
-  useAuthStore,
-  validations,
 };
