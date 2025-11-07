6da4bc30f32b0f654e926fc7426b823c9b584815
Alejandro Aspinwall <aaspinwall@gmail.com>
2025-11-07 09:59:38 -0500

    name thunderbird pro

--- packages/addon/scripts/config.ts
+++ packages/addon/scripts/config.ts
@@ -4,8 +4,8 @@ dotenv.config();
 export const ID_FOR_PROD = `"id": "tbpro-add-on@thunderbird.net"`;
 export const ID_FOR_STAGE = ` "id": "tbpro-addon-stage@thunderbird.net"`;
 
-export const NAME_FOR_PROD = `"name": "Tbpro Addon"`;
-export const NAME_FOR_STAGE = `"name": "Tbpro Addon [STAGE]"`;
+export const NAME_FOR_PROD = `"name": "Thunderbird Pro"`;
+export const NAME_FOR_STAGE = `"name": "Thunderbird Pro [STAGE]"`;
 
 export const PACKAGE_NAME = {
   production: 'tbpro-addon',
