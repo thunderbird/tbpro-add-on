299d64dfe06b69bca9141f825f23fce4c4827b11
Alejandro Aspinwall <aaspinwall@gmail.com>
2025-11-10 17:12:28 -0500

    add sentry to addon

--- packages/addon/src/apps/extension.js
+++ packages/addon/src/apps/extension.js
@@ -1,7 +1,8 @@
 import Extension from 'send-frontend/src/apps/send/ExtensionPage.vue';
 import { createApp } from 'vue';
-import { initSentry } from 'send-frontend/src/lib/sentry';
+
 import { mountApp, setupApp } from 'send-frontend/src/apps/send/setup';
+import { initSentry } from './lib/sentry';
 
 const app = createApp(Extension);
 
--- packages/addon/src/apps/management.js
+++ packages/addon/src/apps/management.js
@@ -2,7 +2,9 @@
 import { mountApp, setupApp } from 'send-frontend/src/apps/send/setup';
 import { createApp } from 'vue';
 import ManagementPage from './AddonManagementPage.vue';
+import { initSentry } from './lib/sentry';
 
 const app = createApp(ManagementPage);
+initSentry(app);
 setupApp(app);
 mountApp(app, '#management-page');
