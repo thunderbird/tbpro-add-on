7e66112e67c06c40e1ad18d1bd99cf6624c55ed5
Alejandro Aspinwall <aaspinwall@gmail.com>
2025-11-11 18:09:49 -0500

    style auth

---
+++ packages/send/frontend/src/apps/common/RightArrowIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M12.663 14.75a.833.833 0 0 0 1.178.011l4.243-4.166a.833.833 0 0 0 0-1.19L13.84 5.24a.833.833 0 1 0-1.167 1.189l2.788 2.739H2.5a.833.833 0 1 0 0 1.666h12.962l-2.788 2.74a.833.833 0 0 0-.011 1.178z"
+      fill="#1A202C"
+    />
+  </svg>
+</template>
--- packages/send/frontend/src/lib/auth/AuthButtons.vue
+++ packages/send/frontend/src/lib/auth/AuthButtons.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import FeedbackBox from '@send-frontend/apps/common/FeedbackBox.vue';
+import RightArrowIcon from '@send-frontend/apps/common/RightArrowIcon.vue';
 import { PrimaryButton } from '@thunderbirdops/services-ui';
 import { ref } from 'vue';
 
@@ -13,29 +14,49 @@ defineProps<{
 const loginText = ref('Sign In');
 </script>
 
-<template class="auth-buttons">
-  <PrimaryButton
-    v-if="!isExtension"
-    data-testid="login-button-tbpro"
-    @click.capture="loginToOidc"
-    >{{ loginText }}</PrimaryButton
-  >
-  <PrimaryButton
-    v-if="isExtension"
-    data-testid="extension-management-button"
-    @click.capture="loginToOidcForExtension"
-    >{{ loginText }}</PrimaryButton
-  >
+<template>
+  <div class="auth-buttons">
+    <PrimaryButton
+      v-if="!isExtension"
+      data-testid="login-button-tbpro"
+      @click.capture="loginToOidc"
+    >
+      <div class="buttons-container">
+        <span class="button-text">
+          {{ loginText }}
+        </span>
+        <RightArrowIcon />
+      </div>
+    </PrimaryButton>
+
+    <PrimaryButton
+      v-if="isExtension"
+      data-testid="extension-management-button"
+      @click.capture="loginToOidcForExtension"
+    >
+      <div class="buttons-container">
+        <span class="button-text">
+          {{ loginText }}
+        </span>
+        <RightArrowIcon />
+      </div>
+    </PrimaryButton>
+  </div>
   <FeedbackBox />
 </template>
 
 <style scoped>
-.auth-buttons {
-  margin: 1rem;
+.buttons-container {
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+  justify-content: center;
+  align-items: center;
+  max-width: 300px;
+  padding: 0 1rem;
 }
-.auth-buttons button {
+.button-text {
   font-size: 13px;
   font-weight: 600;
-  max-width: 300px;
 }
 </style>
